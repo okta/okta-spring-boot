17b60363b42cf8efcdb39592ca4ab04d0c7008e1
Brian Demers <bdemers@apache.org>
2017-10-01 07:30:52 -0400

    Move new ITs into separate module and record coverage

--- integration-tests/oauth2/src/test/groovy/com/okta/spring/tests/wiremock/QuickHttpMockTest.groovy
+++ integration-tests/oauth2/src/test/groovy/com/okta/spring/tests/wiremock/QuickHttpMockTest.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.okta.spring.example.wiremock
+package com.okta.spring.tests.wiremock
 
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.okta.spring.oauth.discovery.OidcDiscoveryClient
