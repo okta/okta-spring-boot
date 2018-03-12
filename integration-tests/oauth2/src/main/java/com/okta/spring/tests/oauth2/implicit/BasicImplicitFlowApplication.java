8f519e8f084604c16d699f2ed4517285e117e113
Brian Demers <bdemers@apache.org>
2018-03-12 14:10:40 -0400

    Update the oidc-tck to 0.2.2
    The example app needed to change to support this (this was back ported from the okta spring samples repo

--- integration-tests/oauth2/src/main/java/com/okta/spring/tests/oauth2/implicit/BasicImplicitFlowApplication.java
+++ integration-tests/oauth2/src/main/java/com/okta/spring/tests/oauth2/implicit/BasicImplicitFlowApplication.java
@@ -17,21 +17,35 @@ package com.okta.spring.tests.oauth2.implicit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-@RestController
 @EnableResourceServer
 @SpringBootApplication
 public class BasicImplicitFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicImplicitFlowApplication.class, args);
+    }
 
     @EnableGlobalMethodSecurity(prePostEnabled = true)
     protected static class GlobalSecurityConfiguration extends GlobalMethodSecurityConfiguration {
@@ -41,19 +55,60 @@ public class BasicImplicitFlowApplication {
         }
     }
 
-    @GetMapping("/")
-    @PreAuthorize("#oauth2.hasScope('email')")
-    public String getMessageOfTheDay(Principal principal) {
-        return "The message of the day is boring: " + principal.getName();
+    @Configuration
+    @Order(0)
+    static class ResourceSecurityConfigurer extends ResourceServerConfigurerAdapter {
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                    .anyRequest().authenticated();
+        }
     }
 
-    @GetMapping("/everyone")
-    @PreAuthorize("hasAuthority('Everyone')")
-    public String everyoneAccess(Principal principal) {
-        return "Everyone has Access: " + principal.getName();
+    @RestController
+    @CrossOrigin(origins = "http://localhost:8080")
+    public static class MessageOfTheDayController {
+
+        @GetMapping("/api/userProfile")
+        @PreAuthorize("#oauth2.hasScope('profile')")
+        public Map<String, Object> getUserDetails(OAuth2Authentication authentication) {
+            return (Map<String, Object>) authentication.getUserAuthentication().getDetails();
+        }
+
+        @GetMapping("/api/messages")
+        @PreAuthorize("#oauth2.hasScope('email')")
+        public Map<String, Object> messages() {
+
+            Map<String, Object> result = new HashMap<>();
+            result.put("messages", Arrays.asList(
+                    new Message("I am a robot."),
+                    new Message("Hello, word!")
+            ));
+
+            return result;
+        }
+
+        @GetMapping("/")
+        @PreAuthorize("#oauth2.hasScope('email')")
+        public String getMessageOfTheDay(Principal principal) {
+            return "The message of the day is boring: " + principal.getName();
+        }
+
+        @GetMapping("/everyone")
+        @PreAuthorize("hasAuthority('Everyone')")
+        public String everyoneAccess(Principal principal) {
+            return "Everyone has Access: " + principal.getName();
+        }
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(BasicImplicitFlowApplication.class, args);
+    static class Message {
+        public Date date = new Date();
+        public String text;
+
+        Message(String text) {
+            this.text = text;
+        }
     }
 }
