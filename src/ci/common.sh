d4ff39e6888dbc1aa92b9877f385f92dd4a4d2e1
Brian Demers <bdemers@apache.org>
2018-09-27 16:59:56 -0400

    fix var expansion based on review feedback

--- src/ci/common.sh
+++ src/ci/common.sh
@@ -28,7 +28,7 @@ PULL_REQUEST=${PULL_REQUEST:-${TRAVIS_PULL_REQUEST:-true}} # default to true
 BRANCH=${TRAVIS_BRANCH:-"$(git rev-parse --abbrev-ref HEAD)"}
 
 # run the ITs if we have an ENV_VARS are set
-if [ "$TRAVIS_SECURE_ENV_VARS" = true ] ; then
+if [ "${TRAVIS_SECURE_ENV_VARS}" = true ] ; then
     RUN_ITS=true
 fi
 RUN_ITS=${RUN_ITS:-false}
