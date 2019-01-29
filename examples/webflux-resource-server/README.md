6f34c2bf6c047f3b540937ec55561d659c1f2147
Brian Demers <bdemers@apache.org>
2019-01-29 13:20:23 -0500

    Add basic reactive resource server example

---
+++ examples/webflux-resource-server/README.md
@@ -0,0 +1,10 @@
+Run the example:
+``` bash
+mvn spring-boot:run \
+ -Dokta.oauth2.issuer=https://{yourOktaDomain}/oauth2/{yourAuthorizationServerId} \
+ -Dokta.oauth2.audience={yourAuthorizationServerAudience} \
+ -Dokta.oauth2.clientId={oauthClientId} \
+ -Dokta.oauth2.groupsClaim={customRoleClaim) # defaults to 'groups'
+```
+
+Browse to: http://localhost:8080
---
+++ examples/webflux-resource-server/src/main/java/com/okta/spring/example/webflux/ReactiveApplication.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2017 Okta, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.okta.spring.example.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@RestController
+@EnableReactiveMethodSecurity
+public class ReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveApplication.class, args);
+    }
+
+    @GetMapping("/userdetails")
+    public Mono<OAuth2AuthenticationToken> userDetailsMono(Mono<OAuth2AuthenticationToken> token) {
+        return token;
+    }
+
+    @EnableWebFluxSecurity
+    static class SecurityConfiguration {
+
+        @Bean
+        public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+            return http
+                .authorizeExchange()
+                    .anyExchange().authenticated()
+                    .and()
+                .oauth2Login()
+                    .and()
+                .oauth2ResourceServer()
+                    .jwt().and().and().build();
+            }
+    }
+}
