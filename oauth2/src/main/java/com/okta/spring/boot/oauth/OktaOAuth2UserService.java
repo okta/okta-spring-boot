86828d06237bf53a5329677c836036977210f8b1
Brian Demers <bdemers@apache.org>
2019-01-29 13:17:12 -0500

    Pull authority parsing logic out to allow for reuse

--- oauth2/src/main/java/com/okta/spring/boot/oauth/OktaOAuth2UserService.java
+++ oauth2/src/main/java/com/okta/spring/boot/oauth/OktaOAuth2UserService.java
@@ -16,17 +16,12 @@
 package com.okta.spring.boot.oauth;
 
 import com.okta.spring.boot.oauth.http.UserAgentRequestInterceptor;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.HashSet;
-import java.util.Set;
 
 final class OktaOAuth2UserService extends DefaultOAuth2UserService {
 
@@ -46,24 +41,7 @@ final class OktaOAuth2UserService extends DefaultOAuth2UserService {
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) {
-
         OAuth2User user = super.loadUser(userRequest);
-
-        // Only post process requests from the "Okta" reg
-        if (!"Okta".equals(userRequest.getClientRegistration().getClientName())) {
-            return user;
-        }
-
-        // start with authorities from super
-        Set<GrantedAuthority> authorities = new HashSet<>(user.getAuthorities());
-        // add 'SCOPE_' authorities
-        authorities.addAll(TokenUtil.tokenScopesToAuthorities(userRequest.getAccessToken()));
-        // add any authorities extracted from the 'group' claim
-        authorities.addAll(TokenUtil.tokenClaimsToAuthorities(user.getAttributes(), groupClaim));
-
-        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
-                .getUserInfoEndpoint().getUserNameAttributeName();
-
-        return new DefaultOAuth2User(authorities, user.getAttributes(), userNameAttributeName);
+        return UserUtil.decorateUser(user, userRequest, groupClaim);
     }
 }
---
+++ oauth2/src/main/java/com/okta/spring/boot/oauth/UserUtil.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2018-Present Okta, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.okta.spring.boot.oauth;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.util.StringUtils;
+
+import java.util.HashSet;
+import java.util.Set;
+
+final class UserUtil {
+
+    private UserUtil() {}
+
+
+    static OAuth2User decorateUser(OAuth2User user, OAuth2UserRequest userRequest, String groupClaim) {
+
+        // Only post process requests from the "Okta" reg
+        if (!"Okta".equals(userRequest.getClientRegistration().getClientName())) {
+            return user;
+        }
+
+        // start with authorities from super
+        Set<GrantedAuthority> authorities = new HashSet<>(user.getAuthorities());
+        // add 'SCOPE_' authorities
+        authorities.addAll(TokenUtil.tokenScopesToAuthorities(userRequest.getAccessToken()));
+        // add any authorities extracted from the 'group' claim
+        authorities.addAll(TokenUtil.tokenClaimsToAuthorities(user.getAttributes(), groupClaim));
+
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
+                .getUserInfoEndpoint().getUserNameAttributeName();
+
+        return new DefaultOAuth2User(authorities, user.getAttributes(), userNameAttributeName);
+    }
+
+    static OidcUser decorateUser(OidcUser user, OidcUserRequest userRequest, String groupClaim) {
+
+        // Only post process requests from the "Okta" reg
+        if (!"Okta".equals(userRequest.getClientRegistration().getClientName())) {
+            return user;
+        }
+
+        // start with authorities from super
+        Set<GrantedAuthority> authorities = new HashSet<>(user.getAuthorities());
+        // add 'SCOPE_' authorities
+        authorities.addAll(TokenUtil.tokenScopesToAuthorities(userRequest.getAccessToken()));
+        // add any authorities extracted from the 'group' claim
+        authorities.addAll(TokenUtil.tokenClaimsToAuthorities(user.getAttributes(), groupClaim));
+
+        String userNameAttributeName = userRequest.getClientRegistration()
+            .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        return StringUtils.hasText(userNameAttributeName)
+                ? new DefaultOidcUser(authorities, user.getIdToken(), user.getUserInfo(), userNameAttributeName)
+                : new DefaultOidcUser(authorities, user.getIdToken(), user.getUserInfo());
+    }
+}
