3c9a45cbb2e717ae9ec8b44ea842a3144e952dc3
Travis CI - Auto Doc Build (from Travis CI) <developers@okta.com>
2019-06-04 21:07:35 +0000

    Deploy okta/okta-spring-boot to github.com/okta/okta-spring-boot.git:gh-pages

---
+++ versions.js
@@ -0,0 +1,12 @@
+window.apiVersions = {
+  name: "Okta Spring Boot Starter",
+  url: "https://github.com/okta/okta-spring-boot",
+  versions: [
+    '0.4.1',
+    '0.4.0',
+    '0.3.0',
+    '0.2.0',
+    '0.1.0'
+  ],
+  current: '0.4.1'
+};
