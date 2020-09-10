5d510361fdab3455c15cfb4f1bbfa53288f4528a
Arvind Krishnakumar <61501885+arvindkrishnakumar-okta@users.noreply.github.com>
2020-09-10 13:58:56 -0700

    updated javadoc comment for opaque

--- oauth2/src/main/java/com/okta/spring/boot/oauth/config/OktaOAuth2Properties.java
+++ oauth2/src/main/java/com/okta/spring/boot/oauth/config/OktaOAuth2Properties.java
@@ -72,7 +72,8 @@ public final class OktaOAuth2Properties implements Validator {
     private String postLogoutRedirectUri;
 
     /**
-     * Signals need for opaque token validation/introspection (if set to true).
+     * Flag to indicate the need for opaque token validation/introspection (if set to true).
+     * If this is not set (or) set to false, opaque introspection will not be performed.
      */
     private boolean opaque = false;
 
