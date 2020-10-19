27fd0a6c451272ca41e21ea1425e9ea1090ba127
Arvind Krishnakumar <61501885+arvindkrishnakumar-okta@users.noreply.github.com>
2020-10-19 09:13:52 -0700

    added integration tests and refactored test class names

--- integration-tests/oauth2/src/test/groovy/com/okta/spring/tests/oauth2/opaquetoken/ResourceServerOpaqueTokenValidationIT.groovy
+++ integration-tests/oauth2/src/test/groovy/com/okta/spring/tests/oauth2/opaquetoken/ResourceServerOpaqueTokenValidationIT.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-Present Okta, Inc.
+ * Copyright 2017 Okta, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,32 +13,46 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.okta.spring.tests.oauth2.implicit
+package com.okta.spring.tests.oauth2.opaquetoken
 
 import com.okta.test.mock.Scenario
 import com.okta.test.mock.application.ApplicationTestRunner
+import io.restassured.http.ContentType
 import org.hamcrest.Matchers
 import org.testng.annotations.Test
 
 import static com.okta.test.mock.scenarios.Scenario.IMPLICIT_FLOW_REMOTE_VALIDATION
 import static io.restassured.RestAssured.given
+import static org.hamcrest.Matchers.startsWith
 
 @Scenario(IMPLICIT_FLOW_REMOTE_VALIDATION)
-class ImplicitRemoteValidationGroupIT extends ApplicationTestRunner {
+class ResourceServerOpaqueTokenValidationIT extends ApplicationTestRunner {
 
     private final static String ERROR_401 = "401 Unauthorized"
 
     @Test
     void groupAccessTest() {
         given()
-            .header("Authorization", "Bearer ${IMPLICIT_FLOW_REMOTE_VALIDATION.definition.accessTokenJwt}")
+            .header("Authorization", "accessTokenJwt_groups")
             .redirects()
                 .follow(false)
         .when()
             .get("http://localhost:${applicationPort}/everyone")
         .then()
-            .statusCode(200)
             .body(Matchers.equalTo("Everyone has Access: joe.coder@example.com"))
     }
 
+    @Test
+    void test401ResponseBody() {
+         given()
+            .contentType(ContentType.ANY)
+            .redirects()
+                .follow(false)
+        .when()
+            .get("http://localhost:${applicationPort}/everyone")
+        .then()
+            .statusCode(401)
+            .header("WWW-Authenticate", startsWith("Bearer"))
+            .body(Matchers.equalTo(ERROR_401))
+    }
 }
