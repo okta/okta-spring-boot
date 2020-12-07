67f3c97d7a0dd626e6935eb359fb6e8aac8b9b64
Arvind Krishnakumar <61501885+arvindkrishnakumar-okta@users.noreply.github.com>
2020-12-07 13:01:19 -0800

    add username and password to proxy config

--- oauth2/src/main/java/com/okta/spring/boot/oauth/config/OktaOAuth2Properties.java
+++ oauth2/src/main/java/com/okta/spring/boot/oauth/config/OktaOAuth2Properties.java
@@ -78,6 +78,10 @@ public final class OktaOAuth2Properties implements Validator {
 
     private int proxyPort;
 
+    private String proxyUser;
+
+    private String proxyPassword;
+
     // work around for https://github.com/spring-projects/spring-boot/issues/17035
     private OktaOAuth2Properties() {
         this(null);
@@ -175,6 +179,22 @@ public final class OktaOAuth2Properties implements Validator {
 
     public void setProxyPort(int proxyPort) {
         this.proxyPort = proxyPort;
+    }
+
+    public String getProxyUser() {
+        return proxyUser;
+    }
+
+    public void setProxyUser(String proxyUser) {
+        this.proxyUser = proxyUser;
+    }
+
+    public String getProxyPassword() {
+        return proxyPassword;
+    }
+
+    public void setProxyPassword(String proxyPassword) {
+        this.proxyPassword = proxyPassword;
     }
 
     @Override
